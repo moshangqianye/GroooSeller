5d1324b0e968d69c2d186bb52fce28b132a7e812
runzii <806478101@qq.com>
2016-10-06 18:03:40 +0800

    修复消失的代码

---
+++ library/src/main/java/com/runzii/lib/app/MyActions.java
@@ -0,0 +1,10 @@
+package com.runzii.lib.app;
+
+/**
+ * Created by runzii on 16-10-6.
+ */
+
+public class MyActions {
+    public static final String ACTION_PLAY = "1";
+    public static final String ACTION_PAUSE = "2";
+}
